63d5f7a85601b26975c6f9703e511b0ce1457018
Víctor Alfonso Montero Javier <victor.alf-11@hotmail.com>
2020-01-11 21:48:56 -0400

    Agregadas una base de datos y su referencia. Funcionaria en cualquier maquina.

--- Banco01.MDC/Form1.Designer.cs
+++ Banco01.MDC/Form1.Designer.cs
@@ -28,12 +28,100 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.mDC_Test01_DataSet = new Banco01.MDC.Resources.MDC_Test01_DataSet();
+            this.mDCTest01DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tableTestBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tableTestTableAdapter = new Banco01.MDC.Resources.MDC_Test01_DataSetTableAdapters.TableTestTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mDC_Test01_DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mDCTest01DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableTestBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tableTestBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(66, 98);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // mDC_Test01_DataSet
+            // 
+            this.mDC_Test01_DataSet.DataSetName = "MDC_Test01_DataSet";
+            this.mDC_Test01_DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // mDCTest01DataSetBindingSource
+            // 
+            this.mDCTest01DataSetBindingSource.DataSource = this.mDC_Test01_DataSet;
+            this.mDCTest01DataSetBindingSource.Position = 0;
+            // 
+            // tableTestBindingSource
+            // 
+            this.tableTestBindingSource.DataMember = "TableTest";
+            this.tableTestBindingSource.DataSource = this.mDCTest01DataSetBindingSource;
+            // 
+            // tableTestTableAdapter
+            // 
+            this.tableTestTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            this.descriptionDataGridViewTextBoxColumn.ReadOnly = true;
+            this.descriptionDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mDC_Test01_DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mDCTest01DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableTestBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource mDCTest01DataSetBindingSource;
+        private Resources.MDC_Test01_DataSet mDC_Test01_DataSet;
+        private System.Windows.Forms.BindingSource tableTestBindingSource;
+        private Resources.MDC_Test01_DataSetTableAdapters.TableTestTableAdapter tableTestTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
     }
 }
 
--- Banco01.MDC/Form1.cs
+++ Banco01.MDC/Form1.cs
@@ -16,5 +16,12 @@ namespace Banco01.MDC
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'mDC_Test01_DataSet.TableTest' table. You can move, or remove it, as needed.
+            this.tableTestTableAdapter.Fill(this.mDC_Test01_DataSet.TableTest);
+
+        }
     }
 }
