fe5792d3aa28a10d62d268ccb10578d236d2e196
Víctor A. Montero <vamj809@users.noreply.github.com>
2020-01-15 11:22:07 -0400

    Ajustando cuadres y base de datos

--- Banco01.MDC/Cuadre/CentroCuadres.Designer.cs
+++ Banco01.MDC/Cuadre/CentroCuadres.Designer.cs
@@ -28,31 +28,43 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.Fecha_Inicio = new System.Windows.Forms.DateTimePicker();
+            this.Fecha_Fin = new System.Windows.Forms.DateTimePicker();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.detallesCuadreBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.nombreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.fechaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.horaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tipoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.montoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.detallesCuadreBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.detallesCuadreBindingSource)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dateTimePicker1
+            // Fecha_Inicio
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(343, 26);
-            this.dateTimePicker1.TabIndex = 0;
+            this.Fecha_Inicio.CustomFormat = "MMM dd, yyyy";
+            this.Fecha_Inicio.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.Fecha_Inicio.Location = new System.Drawing.Point(71, 22);
+            this.Fecha_Inicio.Name = "Fecha_Inicio";
+            this.Fecha_Inicio.Size = new System.Drawing.Size(181, 26);
+            this.Fecha_Inicio.TabIndex = 0;
+            this.Fecha_Inicio.Value = new System.DateTime(2020, 1, 15, 10, 52, 27, 0);
+            this.Fecha_Inicio.ValueChanged += new System.EventHandler(this.Fechas_ValueChanged);
             // 
-            // dateTimePicker2
+            // Fecha_Fin
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(548, 12);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(343, 26);
-            this.dateTimePicker2.TabIndex = 1;
+            this.Fecha_Fin.CustomFormat = "MMM dd, yyyy";
+            this.Fecha_Fin.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.Fecha_Fin.Location = new System.Drawing.Point(360, 22);
+            this.Fecha_Fin.Name = "Fecha_Fin";
+            this.Fecha_Fin.Size = new System.Drawing.Size(180, 26);
+            this.Fecha_Fin.TabIndex = 1;
+            this.Fecha_Fin.Value = new System.DateTime(2020, 1, 15, 10, 52, 21, 0);
+            this.Fecha_Fin.ValueChanged += new System.EventHandler(this.Fechas_ValueChanged);
             // 
             // dataGridView1
             // 
@@ -62,39 +74,36 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.nombreDataGridViewTextBoxColumn,
-            this.fechaDataGridViewTextBoxColumn,
+            this.horaDataGridViewTextBoxColumn,
             this.tipoDataGridViewTextBoxColumn,
             this.montoDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.detallesCuadreBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 44);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 62);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(879, 304);
+            this.dataGridView1.Size = new System.Drawing.Size(903, 333);
             this.dataGridView1.TabIndex = 2;
-            // 
-            // detallesCuadreBindingSource
-            // 
-            this.detallesCuadreBindingSource.DataSource = typeof(Banco01.MDC.Resources.DetallesCuadre);
             // 
             // nombreDataGridViewTextBoxColumn
             // 
             this.nombreDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.nombreDataGridViewTextBoxColumn.DataPropertyName = "Nombre";
-            this.nombreDataGridViewTextBoxColumn.HeaderText = "Cajero que le atendió";
+            this.nombreDataGridViewTextBoxColumn.HeaderText = "Nombre";
             this.nombreDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.nombreDataGridViewTextBoxColumn.Name = "nombreDataGridViewTextBoxColumn";
             this.nombreDataGridViewTextBoxColumn.ReadOnly = true;
             // 
-            // fechaDataGridViewTextBoxColumn
+            // horaDataGridViewTextBoxColumn
             // 
-            this.fechaDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.fechaDataGridViewTextBoxColumn.DataPropertyName = "Fecha";
-            this.fechaDataGridViewTextBoxColumn.HeaderText = "Fecha";
-            this.fechaDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.fechaDataGridViewTextBoxColumn.Name = "fechaDataGridViewTextBoxColumn";
-            this.fechaDataGridViewTextBoxColumn.ReadOnly = true;
+            this.horaDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.horaDataGridViewTextBoxColumn.DataPropertyName = "Hora";
+            this.horaDataGridViewTextBoxColumn.HeaderText = "Fecha/Hora";
+            this.horaDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.horaDataGridViewTextBoxColumn.Name = "horaDataGridViewTextBoxColumn";
+            this.horaDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // tipoDataGridViewTextBoxColumn
             // 
@@ -114,34 +123,73 @@
             this.montoDataGridViewTextBoxColumn.Name = "montoDataGridViewTextBoxColumn";
             this.montoDataGridViewTextBoxColumn.ReadOnly = true;
             // 
+            // detallesCuadreBindingSource
+            // 
+            this.detallesCuadreBindingSource.DataSource = typeof(Banco01.MDC.Resources.DetallesCuadre);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Fecha_Inicio);
+            this.panel1.Controls.Add(this.Fecha_Fin);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(903, 62);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inicio:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(317, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fin:";
+            // 
             // CentroCuadres
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(903, 395);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.dateTimePicker2);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "CentroCuadres";
-            this.Text = "CentroCuadres";
+            this.Text = "Cuadre de Entrada/Salida de Efectivo en Caja";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CentroCuadres_FormClosing);
             this.Load += new System.EventHandler(this.CentroCuadres_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.detallesCuadreBindingSource)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker Fecha_Inicio;
+        private System.Windows.Forms.DateTimePicker Fecha_Fin;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.BindingSource detallesCuadreBindingSource;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.DataGridViewTextBoxColumn nombreDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn fechaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn horaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn tipoDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn montoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource detallesCuadreBindingSource;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Banco01.MDC/Cuadre/CentroCuadres.cs
+++ Banco01.MDC/Cuadre/CentroCuadres.cs
@@ -11,14 +11,20 @@ using System.Windows.Forms;
 using log4net;
 using Banco01.MDC.Resources;
 using System.Data.Entity;
+using Banco01.MDC.Cajero;
 
 namespace Banco01.MDC.Cuadre
 {
     public partial class CentroCuadres : Form
     {
         private static readonly ILog Logger = LogManager.GetLogger(System.Environment.MachineName);
-        public CentroCuadres()
+        private readonly ValidaCajero_Result CurrentUser;
+        public CentroCuadres(ValidaCajero_Result _currentUser = null)
         {
+            if (_currentUser == null) {
+                _currentUser = new CajeroEspecial();
+            }
+            CurrentUser = _currentUser;
             InitializeComponent();
         }
 
@@ -27,6 +33,32 @@ namespace Banco01.MDC.Cuadre
             MDC_LocalDBEntities localDBEntity = new MDC_LocalDBEntities();
             localDBEntity.DetallesCuadre.Load();
             this.detallesCuadreBindingSource.DataSource = localDBEntity.DetallesCuadre.Local.ToBindingList();
+            dataGridView1.Sort(dataGridView1.Columns["Hora"], ListSortDirection.Ascending);
+            
+            //Toma todas las fechas, y busca la más antigua y la más reciente.
+            var dateTimes = dataGridView1.Rows.Cast<DataGridViewRow>()
+                            .Select(x => (DateTime) x.Cells["Hora"].Value);
+            var minValue = dateTimes.Min();
+            var maxValue = dateTimes.Max();
+
+            Fecha_Inicio.Value = maxValue;
+            Fecha_Inicio.MinDate = minValue;
+            Fecha_Inicio.MaxDate = maxValue;
+
+            Fecha_Fin = Fecha_Inicio;
+            //// Manejo de Fechas ^^^
+        }
+
+        private void CentroCuadres_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CajaMenuPrincipal mm = new CajaMenuPrincipal(CurrentUser);
+            this.Hide();
+            mm.Show();
+        }
+
+        private void Fechas_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
